adeabf155fe08200e8be2acbe1c09de24cb9add7
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2023-08-12 09:11:08 +0000

    datatypes notes

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node js ,not  browser
+
+let name = "chinmay" ;
+let age = 22 ;
+let isLoggedIn = true ;
+
+// number => 2 to power 53
+// bigint 
+// string => " "
+// boolean => true/false
+// NULL => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "hitesh"); // gives type od datatype 
+
+console.log(typeof null); // null is a object type (returns object)
+
+console.log(typeof undefined); //  undefined is returns undefined 
