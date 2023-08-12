38a4e33b9951d37a46fa3cd2d10f0f71523f6f22
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2023-08-12 17:56:02 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,21 @@
+const name = "chinmay " ;
+const repoCount = 2 ;
+
+// console.log(name + repoCount + " keni");  --> too old 
+
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+// anothe way to declare string
+const gameName = new String("counter-strike") ;
+// console.log(gameName);
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf(m));
+
+const newStr = gameName.substring(0,4) ;
+console.log(newStr); 
