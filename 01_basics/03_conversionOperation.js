7dee3e1e06e7d4aafeaace7300d69510a591b00b
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2023-08-12 10:07:33 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "22hfn" ;
+// let score = null ; // convert to 0
+// let score = undefined ; // conert to Nan
+// let score = true ; // conert to 1 and if there is false then 0
+
+// "33" => 33 
+// "33uyn" => Nan
+// true =>  ; false => 0 ;
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score) ; // conversion
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+// let isLoggedIn = "chinmay" ;
+// let booleanIsLoggedIn = Boolean(isLoggedIn) ;
+// console.log(booleanIsLoggedIn);
+
+// 1=>true; 0=>false ;
+// "" => false
+// "chinmay" => true 
+
+let someNumber = 33 ;
+let stringNumber = String(someNumber) ;
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
