640f7364441b55cf80d9159619547d5d8e2659fd
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2023-08-12 10:34:40 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,7 +25,40 @@ let score = "22hfn" ;
 
 let someNumber = 33 ;
 let stringNumber = String(someNumber) ;
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
 
+// ******************** Operations *************************
+let value = 3 ;
+let negValue = -value ;
+// console.log(negValue) ;
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+
+let str1 = "chinmay " ;
+let str2 = "keni" ;
+let str3 = str1 + str2 ;
+// console.log(str3) ;
+
+// console.log("1" + 2) ;
+// console.log(1 + "2") ;
+// console.log("1" + 2 + 2) ;
+// console.log(1 + 2 + "3") ;
+
+// console.log(+true);
+// console.log(+"");
+
+let x = 3 ;
+let y = x++ ;
+let r = x ;
+let z = ++x ;
+
+console.log(y);
+console.log(r);
+console.log(z);
 
