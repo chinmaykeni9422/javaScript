c144c665506f73bbaa9e8e28d20b2f4cbccd6e3d
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2023-08-20 17:17:40 +0530

    hoisting and scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,10 +5,53 @@ if(true){
     let a = 10 ;
     const b = 20 ;
     var c = 30 ;
-    console.log(`INNER ${a}`) ;
+    // console.log(`INNER ${a}`) ;
 }
 
 
-console.log(a) ;  // error -> not define
+// console.log(a) ;  // error -> not define
 // console.log(b) ;  // error -> not define
-console.log(c) ; // no error
+// console.log(c) ; // no error
+
+
+
+//-----------------------------------------------------------------------------
+
+function one(){
+    const username = "chinmay" ;
+    function two(){
+        const website = "youtube" ;
+        console.log(username) ;
+    }
+    //console.log(website) ;
+    two() ;
+}
+
+// one() ;
+
+
+if(true){
+    const user = "chinmay" ;
+    if(true){
+        const website = " youtube" ;
+        //console.log(user+website) ;
+    }
+    // console.log(website) ;
+}
+// console.log(user) ;
+
+
+
+//----------------intresting----------------------------------------------
+
+
+console.log(addOne(5)) ;
+function addOne(num){
+    return num+1 ;
+}
+
+
+addTwo(8) ;
+const addTwo = function(num){
+    return num+1 ;
+}
